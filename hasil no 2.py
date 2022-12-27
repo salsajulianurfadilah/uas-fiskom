138cd0006d0e3e21ee65d6989952be4c2f8cd7fa
salsajulianurfadilah <114701778+salsajulianurfadilah@users.noreply.github.com>
2022-12-27 17:58:43 +0700

    Add files via upload

---
+++ hasil no 2.py
@@ -0,0 +1,9 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Bu EEN CANTIK/Documents/PRAK FISKOM/UAS PRAK FISKOM/no 2.py
+Jarak Horizontal Maksimum =  110.46242395209676  m
+Jarak Vertikal Maksimum =  95.66326530612243  m
+Waktu Mencapai Jarak Horizontal Maksimum =  8.83699391616774  s
+
+
